d6eca4fb2bad6eba4c83f672b5d614328accd51d
pbilstein <pbilstein@gmail.com>
2016-05-26 15:00:32 +0200

    added packaging of a bundled tomcat feature

--- soap-mocks/cleanstart.sh
+++ soap-mocks/cleanstart.sh
@@ -1,2 +1,2 @@
 export MAVEN_OPTS="-XX:MaxPermSize=256m -Xmx512m"
-mvn clean install tomcat7:run -Dfile.encoding="UTF-8"
+mvn clean package tomcat7:run -Dfile.encoding="UTF-8"
---
+++ soap-mocks/src/tomcat/bin/setenv.sh
@@ -0,0 +1,1 @@
+JAVA_OPTS="-Dsoapmocks.proxyrecord.dir=$CATALINA_HOME/proxyrecord -Dsoapmocks.files.basedir=$CATALINA_HOME/conf-soapmocks -Dlogback.configurationFile=$CATALINA_HOME/conf-soapmocks/logback.xml"
